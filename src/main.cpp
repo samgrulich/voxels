eb193503db0fee1e7b58c14c79ef0e1b1d378cd5
Sam <samuel.grulich@gmail.com>
2024-03-09 14:32:49 +0100

    Tweaks
    enabled multisampling
    enabled depth testing
    enabled face culling
    added draw lines mode
    changed color picker
    changed clear color
    "temp" added simple camera resize bugfix

--- src/main.cpp
+++ src/main.cpp
@@ -15,12 +15,23 @@
 
 static struct State {
     GLFWwindow* win;
+    glm::ivec2 win_size = {800, 600};
     Camera* cam;
+    bool draw_lines = true;
 } state;
 
 // glfw callbacks
 void resize_callback(GLFWwindow* window, int width, int height);
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
+
+// my funcs
+void change_draw_mode() {
+    if (state.draw_lines) {
+        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    } else {
+        glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+    }
+}
 
 int main(void) {
     /* Initialize the library */
@@ -29,8 +40,8 @@ int main(void) {
 
     /* Create a windowed mode window and its OpenGL context */
     glfwWindowHint(GLFW_RESIZABLE, false);
-    glm::ivec2 win_size(800, 600);
-    state.win = glfwCreateWindow(win_size.x, win_size.y, "Hola", NULL, NULL);
+    glfwWindowHint(GLFW_SAMPLES, 4);
+    state.win = glfwCreateWindow(state.win_size.x, state.win_size.y, "Hola", NULL, NULL);
     if (!state.win) {
         glfwTerminate();
         return -1;
@@ -59,14 +70,19 @@ int main(void) {
     ImGui_ImplOpenGL3_Init();
 
     // my init
-    Camera cam(state.win, (float)win_size.x/win_size.y);
+    Camera cam(state.win, (float)state.win_size.x/state.win_size.y);
     ShaderProgram basicShader("res/shaders/basic.vert", "res/shaders/basic.frag");
     Shape quadShape = cube(1.0);
-    glm::vec4 clearColor = {0.046, 0.046, 0.064, 1.0};
+    glm::vec4 clearColor = {0.7, 0.7, 0.8, 1.0};
 
     state.cam = &cam;
     glClearColor(clearColor.x, clearColor.y, clearColor.z, clearColor.w);
+    glFrontFace(GL_CCW);
+    glCullFace(GL_FRONT);
     glEnable(GL_DEPTH_TEST);
+    glEnable(GL_MULTISAMPLE);
+    glEnable(GL_CULL_FACE);
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(state.win)) {
@@ -75,8 +91,11 @@ int main(void) {
         ImGui_ImplOpenGL3_NewFrame();
         ImGui_ImplGlfw_NewFrame();
         ImGui::NewFrame();
-        if (ImGui::ColorPicker4("Clear color", &clearColor.x)) {
+        if (ImGui::ColorEdit3("Clear color", &clearColor.x)) {
             glClearColor(clearColor.x, clearColor.y, clearColor.z, clearColor.w);
+        }
+        if (ImGui::Checkbox("Draw lines", &state.draw_lines)) {
+            change_draw_mode();
         }
         ImGui::DragFloat3("Position", &cam.position.x, 0.1f);
 
@@ -97,6 +116,12 @@ int main(void) {
 
         /* Poll for and process events */
         glfwPollEvents();
+        glm::ivec2 win_size;
+        glfwGetWindowSize(state.win, &win_size.x, &win_size.y);
+        if (state.win_size != win_size) {
+            glfwGetCursorPos(state.win, &cam.m_cursorX, &cam.m_cursorY);
+            state.win_size = win_size;
+        }
     }
 
     ImGui_ImplOpenGL3_Shutdown();
@@ -107,7 +132,6 @@ int main(void) {
 }
 
 void resize_callback(GLFWwindow* win, int width, int height) {
-    glfwGetCursorPos(state.win, &state.cam->m_cursorX, &state.cam->m_cursorY);
     state.cam->update_aspect((float)width / height);
     glViewport(0, 0, width, height);
 }
@@ -122,5 +146,10 @@ void key_callback(GLFWwindow* win, int key, int scancode, int action, int mods) 
             glfwSetInputMode(win, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
         }
         cursor_hidden = !cursor_hidden;
+    }
+
+    if (key == GLFW_KEY_L && action == GLFW_PRESS) {
+        state.draw_lines = !state.draw_lines;
+        change_draw_mode();
     }
 }
